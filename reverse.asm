660e2986d77258877d78ce179d32d1b2c524f958
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-03-16 10:33:45 +0100

    Update reverse.asm

--- reverse.asm
+++ reverse.asm
@@ -13,77 +13,77 @@
 
 
 
-$ r2 -e bin.cache=true exe  
-[0x00002190]> aaa
-[x] Analyze all flags starting with sym. and entry0 (aa)
-[x] Analyze function calls (aac)
-[x] Analyze len bytes of instructions for references (aar)
-[x] Finding and parsing C++ vtables (avrr)
-[x] Type matching analysis for all functions (aaft)
-[x] Propagate noreturn information (aanr)
-[x] Use -AA or aaaa to perform additional experimental analysis.
-[0x00002190]> afl
-0x00002190    1 34           entry0
-0x0000286c    4 242          method.void_std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_
-0x000021c0    4 41   -> 34   sym.deregister_tm_clones
-0x000021f0    4 57   -> 51   sym.register_tm_clones
-0x00002230    5 57   -> 54   sym.__do_global_dtors_aux
-0x00002180    1 6            sym.imp.__cxa_finalize
-0x00002270    1 9            entry.init0
-0x000023e2    1 291          sym.__static_initialization_and_destruction_0__
-0x000026d6    4 160  -> 134  method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::basic_string_std.allocator_char____char_const__std::allocator_char__const_
-0x0000281c    1 11           method.std::__new_allocator_char_.__new_allocator__
-0x000020a0    1 6            sym.imp.__cxa_atexit
-0x00002140    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::basic_string__
-0x0000253b    1 6            loc.0000253b
-0x00002541    1 11           sym._GLOBAL__sub_I__Z8usernameB5cxx11
-0x00002557    1 35           method.std::char_traits_char_.lt_char_const__char_const_
-0x00002776    5 132          method.bool_std::operator_char__std::char_traits_char___std.allocator_char____std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____const__std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____const
-0x00002279    5 361          main
-0x00002842    3 42           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_::_Guard::_Guard__
-0x00002110    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_dispose__
-0x00002828    1 26           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_::_Guard::_Guard_std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_
-0x00002960    1 9            sym._fini
-0x000027fa    1 33           method.__gnu_cxx::char_traits_char_.eq_char_const__char_const_
-0x0000254c    1 11           method.std.__is_constant_evaluated__
-0x000026b2    1 36           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_Alloc_hider._Alloc_hider__
-0x0000266c    4 70           method.__gnu_cxx::char_traits_char_.length_char_const_
-0x00002000    3 23           sym._init
-0x0000257a   13 192          method.std::char_traits_char_.compare_char_const__char_const__unsigned_long_
-0x0000263a    4 50           method.std::char_traits_char_.length_char_const_
-0x00002030    1 6            sym.imp.strlen
-0x00002040    1 6            sym.imp.memcmp
-0x00002050    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_set_length_unsigned_long_
-0x00002060    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::size___const
-0x00002070    1 6            sym.imp.std::__throw_logic_error_char_const_
-0x00002080    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_local_data__
-0x00002090    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_data_char_
-0x000020b0    1 6            sym.imp.std::basic_ostream_char__std::char_traits_char____std::operator____std::char_traits_char____std::basic_ostream_char__std::char_traits_char_____char_const_
-0x000020c0    1 6            sym.imp.std::ostream::operator___std::ostream____std::ostream__
-0x000020d0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_Alloc_hider::_Alloc_hider_char__std::allocator_char__const_
-0x000020e0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_data___const
-0x000020f0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_S_copy_chars_char__char_const__char_const_
-0x00002100    1 6            sym.imp.exit
-0x00002120    1 6            sym.imp.std::basic_istream_char__std::char_traits_char____std::operator___char__std::char_traits_char___std::allocator_char____std::basic_istream_char__std::char_traits_char_____std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____
-0x00002130    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::data___const
-0x00002150    1 6            sym.imp._Unwind_Resume
-0x00002160    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_create_unsigned_long__unsigned_long_
-0x00002170    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_capacity_unsigned_long_
+; $ r2 -e bin.cache=true exe  
+; [0x00002190]> aaa
+; [x] Analyze all flags starting with sym. and entry0 (aa)
+; [x] Analyze function calls (aac)
+; [x] Analyze len bytes of instructions for references (aar)
+; [x] Finding and parsing C++ vtables (avrr)
+; [x] Type matching analysis for all functions (aaft)
+; [x] Propagate noreturn information (aanr)
+; [x] Use -AA or aaaa to perform additional experimental analysis.
+; [0x00002190]> afl
+; 0x00002190    1 34           entry0
+; 0x0000286c    4 242          method.void_std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_
+; 0x000021c0    4 41   -> 34   sym.deregister_tm_clones
+; 0x000021f0    4 57   -> 51   sym.register_tm_clones
+; 0x00002230    5 57   -> 54   sym.__do_global_dtors_aux
+; 0x00002180    1 6            sym.imp.__cxa_finalize
+; 0x00002270    1 9            entry.init0
+; 0x000023e2    1 291          sym.__static_initialization_and_destruction_0__
+; 0x000026d6    4 160  -> 134  method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::basic_string_std.allocator_char____char_const__std::allocator_char__const_
+; 0x0000281c    1 11           method.std::__new_allocator_char_.__new_allocator__
+; 0x000020a0    1 6            sym.imp.__cxa_atexit
+; 0x00002140    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::basic_string__
+; 0x0000253b    1 6            loc.0000253b
+; 0x00002541    1 11           sym._GLOBAL__sub_I__Z8usernameB5cxx11
+; 0x00002557    1 35           method.std::char_traits_char_.lt_char_const__char_const_
+; 0x00002776    5 132          method.bool_std::operator_char__std::char_traits_char___std.allocator_char____std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____const__std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____const
+; 0x00002279    5 361          main
+; 0x00002842    3 42           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_::_Guard::_Guard__
+; 0x00002110    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_dispose__
+; 0x00002828    1 26           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___._M_construct_char_const__char_const__char_const__std::forward_iterator_tag_::_Guard::_Guard_std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_
+; 0x00002960    1 9            sym._fini
+; 0x000027fa    1 33           method.__gnu_cxx::char_traits_char_.eq_char_const__char_const_
+; 0x0000254c    1 11           method.std.__is_constant_evaluated__
+; 0x000026b2    1 36           method.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_Alloc_hider._Alloc_hider__
+; 0x0000266c    4 70           method.__gnu_cxx::char_traits_char_.length_char_const_
+; 0x00002000    3 23           sym._init
+; 0x0000257a   13 192          method.std::char_traits_char_.compare_char_const__char_const__unsigned_long_
+; 0x0000263a    4 50           method.std::char_traits_char_.length_char_const_
+; 0x00002030    1 6            sym.imp.strlen
+; 0x00002040    1 6            sym.imp.memcmp
+; 0x00002050    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_set_length_unsigned_long_
+; 0x00002060    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::size___const
+; 0x00002070    1 6            sym.imp.std::__throw_logic_error_char_const_
+; 0x00002080    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_local_data__
+; 0x00002090    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_data_char_
+; 0x000020b0    1 6            sym.imp.std::basic_ostream_char__std::char_traits_char____std::operator____std::char_traits_char____std::basic_ostream_char__std::char_traits_char_____char_const_
+; 0x000020c0    1 6            sym.imp.std::ostream::operator___std::ostream____std::ostream__
+; 0x000020d0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_Alloc_hider::_Alloc_hider_char__std::allocator_char__const_
+; 0x000020e0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_data___const
+; 0x000020f0    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_S_copy_chars_char__char_const__char_const_
+; 0x00002100    1 6            sym.imp.exit
+; 0x00002120    1 6            sym.imp.std::basic_istream_char__std::char_traits_char____std::operator___char__std::char_traits_char___std::allocator_char____std::basic_istream_char__std::char_traits_char_____std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char____
+; 0x00002130    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::data___const
+; 0x00002150    1 6            sym.imp._Unwind_Resume
+; 0x00002160    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_create_unsigned_long__unsigned_long_
+; 0x00002170    1 6            sym.imp.std::__cxx11::basic_string_char__std::char_traits_char___std::allocator_char___::_M_capacity_unsigned_long_
 [0x00002190]> s main
 [0x00002279]> axt
 entry0 0x21a4 [DATA] lea rdi, [main]
-[0x00002279]> iz
-[Strings]
-nth paddr      vaddr      len size section type  string
-―――――――――――――――――――――――――――――――――――――――――――――――――――――――
-0   0x00003008 0x00003008 19  20   .rodata ascii Write an username: 
-1   0x00003020 0x00003020 49  50   .rodata ascii The username is correct, now write the password: 
-2   0x00003052 0x00003052 23  24   .rodata ascii The password is correct
-3   0x0000306a 0x0000306a 22  23   .rodata ascii put ur ass out of here
-4   0x00003088 0x00003088 32  33   .rodata ascii seriusly? put ur ass out of here
-5   0x000030b0 0x000030b0 49  50   .rodata ascii basic_string: construction from null is not valid
-6   0x000030e2 0x000030e2 6   7    .rodata ascii Thomas
-7   0x000030e9 0x000030e9 7   8    .rodata ascii Turbato
+; [0x00002279]> iz
+; [Strings]
+; nth paddr      vaddr      len size section type  string
+; ―――――――――――――――――――――――――――――――――――――――――――――――――――――――
+; 0   0x00003008 0x00003008 19  20   .rodata ascii Write an username: 
+; 1   0x00003020 0x00003020 49  50   .rodata ascii The username is correct, now write the password: 
+; 2   0x00003052 0x00003052 23  24   .rodata ascii The password is correct
+; 3   0x0000306a 0x0000306a 22  23   .rodata ascii put ur ass out of here
+; 4   0x00003088 0x00003088 32  33   .rodata ascii seriusly? put ur ass out of here
+; 5   0x000030b0 0x000030b0 49  50   .rodata ascii basic_string: construction from null is not valid
+; 6   0x000030e2 0x000030e2 6   7    .rodata ascii Thomas
+; 7   0x000030e9 0x000030e9 7   8    .rodata ascii Turbato
 
 [0x00002279]> pdr
   ; DATA XREF from entry0 @ 0x21a4
