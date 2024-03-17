3242bada0f231cfbf9e6012f27e9575545a6cf58
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-03-17 15:48:26 +0100

    Update notepad-main.cpp

--- notepad/notepad-main.cpp
+++ notepad/notepad-main.cpp
@@ -1,87 +1,42 @@
 // the code isn't finish yet
 
 
+#include <wx/wx.h>
+#include <wx/notebook.h>
 
 
-
-#include <wx/wx.h>  // Main header of wxWidgets
-#include <wx/textctrl.h>  // For text control
-#include <wx/menu.h>  // For menu bar
-#include <wx/frame.h>  // For main application window
-#include <wx/filedlg.h>  // For file open/save dialog window
-
-// Declaration of the main application class
-class MyNotepadApp : public wxApp {
+class MyFrame : public wxFrame {
 public:
-    virtual bool OnInit();
-};
+    MyFrame() : wxFrame(nullptr, wxID_ANY,"Notepad es", wxDefaultPosition, wxSize(400, 300))
+    {
+        // creare il notebook
+        notebook = new wxNotebook(this, wxID_ANY);
 
-// Declaration of the main window class
-class MyNotepadFrame : public wxFrame {
-public:
-    MyNotepadFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+        // aggiungiamo una pagina vuota
+        notebook->AddPage(new wxPanel(notebook), "Page 1");
+
+        notebook->AddPage(new wxPanel(notebook), "Page 2");
+
+        // creaimo un box sizer per editare il layout
+        wxBoxSizer* Sizer = new wxBoxSizer(wxVERTICAL);
+        Sizer->Add(notebook, 1, wxEXPAND | wxALL, 5); //aggiungiamo la classe notebook al box sixer così queste impostazioni valgono per la finestra
+        SetSizer(Sizer);
+    }
 
 private:
-    // Event handlers
-    void OnOpen(wxCommandEvent& event);
-
-    // Other member variables
-    wxTextCtrl* textCtrl;
+    wxNotebook* notebook;
 };
 
-// Implementation of the application initialization function
-bool MyNotepadApp::OnInit() {
-    // Creation of the main window
-    MyNotepadFrame* frame = new MyNotepadFrame("My Notepad", wxDefaultPosition, wxSize(800, 600));
-
-    // Display the window
-    frame->Show(true);
-
-    return true;
-}
-
-// Implementation of the main window constructor
-MyNotepadFrame::MyNotepadFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-    : wxFrame(NULL, wxID_ANY, title, pos, size)
+class prova : public wxApp
 {
-    // Creation of the text control
-    textCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
-
-    // Creation of the menu bar
-    wxMenu* fileMenu = new wxMenu;
-    fileMenu->Append(wxID_OPEN, "&Open...\tCtrl+O");
-
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(fileMenu, "&File");
-
-    // Associating the menu bar with the window
-    SetMenuBar(menuBar);
-
-    // Event handling for menu items
-    Bind(wxEVT_MENU, &MyNotepadFrame::OnOpen, this, wxID_OPEN);
-}
-
-// Implementation of the event handler for opening a file
-void MyNotepadFrame::OnOpen(wxCommandEvent& event) {
-    wxFileDialog openFileDialog(this, _("Open File"), "", "", "Text files (*.txt)|*.txt", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-
-    if (openFileDialog.ShowModal() == wxID_CANCEL)
-        return;
-
-    wxString filePath = openFileDialog.GetPath();
-    wxFile file(filePath);
-    if (!file.Open(filePath)) {
-        wxLogError("Cannot open file '%s'.", filePath);
-        return;
+public:
+    virtual bool OnInit()
+    {
+        MyFrame* frame = new MyFrame();
+        frame->Show(true);
+        return true;
     }
+};
 
-    wxString fileContent;
-    if (file.ReadAll(&fileContent)) {
-        textCtrl->SetValue(fileContent);
-    } else {
-        wxLogError("Cannot read file '%s'.", filePath);
-    }
-}
+wxIMPLEMENT_APP(prova);
 
-// Entry point of the application
-wxIMPLEMENT_APP(MyNotepadApp);
