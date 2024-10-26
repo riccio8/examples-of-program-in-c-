1814cc192645cd8d5d668f22e2b811f34c585af0
0x90 <134144784+riccio8@users.noreply.github.com>
2024-10-26 11:12:18 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,32 +2,32 @@
 #include <vector>
 #include <cmath>
 
-std::vector<int> trova_divisore(int n) {
-    std::vector<int> divisori;
+std::vector<int> find_divisors(int n) {
+    std::vector<int> divisors;
     int sqrt_n = std::sqrt(n);  
     for (int i = 1; i <= sqrt_n; ++i) {
         if (n % i == 0) {
-            divisori.push_back(i);
+            divisors.push_back(i);
             if (i != 1 && i != n / i) {
-                divisori.push_back(n / i); 
+                divisors.push_back(n / i); 
             }
         }
     }
-    return divisori;
+    return divisors;
 }
 
-void check_moltiplicatore(int n) {
-    std::vector<int> divisori = trova_divisore(n);
-    int somma = 0;
+void check_multiplier(int n) {
+    std::vector<int> divisors = find_divisors(n);
+    int sum = 0;
     
-    for (int d : divisori) {
-        somma += d; 
+    for (int d : divisors) {
+        sum += d; 
     }
 
-    if (somma == n) {
-        std::cout << "FOUNDED NUMBER!! for number n: " << n 
-                  << " divisor: ";
-        for (int d : divisori) {
+    if (sum == n) {
+        std::cout << "FOUND PERFECT NUMBER! for number n: " << n 
+                  << " divisors: ";
+        for (int d : divisors) {
             std::cout << d << " ";
         }
         std::cout << std::endl;
@@ -40,10 +40,10 @@ int main() {
     std::cin >> max_range;
 
     for (int i = 2; i <= max_range; ++i) {
-        check_moltiplicatore(i);
+        check_multiplier(i);
     }
 
     return 0;
 }
 
-// g++ -o perfetto .\test.cpp
+// g++ -o perfect .\test.cpp
